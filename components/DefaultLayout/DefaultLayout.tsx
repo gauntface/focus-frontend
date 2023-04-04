f0a135c77af68fcc36acaec3c4ef962617e6d910
Matt Gaunt-Seo <matt@gaunt.dev>
2023-04-03 19:58:20 -0700

    Add some small style tweaks

--- components/DefaultLayout/DefaultLayout.tsx
+++ components/DefaultLayout/DefaultLayout.tsx
@@ -1,6 +1,5 @@
 import styles from "./DefaultLayout.module.css";
 
-import Link from 'next/link';
 import { User } from "firebase/auth";
 
 import Footer from '../Footer/Footer';
@@ -8,19 +7,10 @@ import TaskHeader from '../../components/TaskHeader/TaskHeader';
 
 export default function DefaultLayout(props: Props) {
 	return <>
-		<TaskHeader user={props.user} date={props.date} selectedView='day' />
-
 		<div className={styles['l-default']}>
-			<div className={styles['l-default__loading']}>
+			<div className={styles['l-default__header']}>
+				<TaskHeader user={props.user} date={props.date} selectedView='day' />
 			</div>
-
-			<header className={styles['l-default__header']}>
-				<Link href="/home" className={styles['l-default__home']}>
-            Home
-				</Link>
-				<h1>{props.title}</h1>
-				{props.subtitle && <h2>{props.subtitle}</h2>}
-			</header>
 			<main className={styles['l-default__main']}>{props.children}</main>
 			<Footer />
 		</div>
