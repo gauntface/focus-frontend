d42e0280600d2e742b3169cefa57c48cce0bbd6f
Matt Gaunt-Seo <matt@gaunt.dev>
2023-04-23 15:44:41 -0700

    Fix linting issues

--- __tests__/components/QuarterTracker.test.tsx
+++ __tests__/components/QuarterTracker.test.tsx
@@ -4,14 +4,14 @@ import '@testing-library/jest-dom'
 import moment from 'moment';
 
 jest.mock('moment', () => {
-    const actualMoment = jest.requireActual('moment');
+	const actualMoment = jest.requireActual('moment');
 	return (date: string) => {
-        if (date) {
-            return actualMoment(date);
-        }
-        // Make it act as though it's a specific day instead of "now"
-        return actualMoment("2006-01-01");
-    }
+		if (date) {
+			return actualMoment(date);
+		}
+		// Make it act as though it's a specific day instead of "now"
+		return actualMoment("2006-01-01");
+	}
 });
 
 describe('QuarterTracker', () => {
@@ -20,26 +20,26 @@ describe('QuarterTracker', () => {
 
 		// Check sign in button
 		const progressText = screen.getByText("Progress in Q1 '06");
-        expect(progressText).toBeTruthy();
+		expect(progressText).toBeTruthy();
 
-        const weeksText = screen.getByText("1 of 12");
-        expect(weeksText).toBeTruthy();
+		const weeksText = screen.getByText("1 of 12");
+		expect(weeksText).toBeTruthy();
 
-        const progressBar = container.querySelector('.c-qt__prog') as HTMLElement;
-        expect(progressBar?.style.width).toEqual('0%');
+		const progressBar = container.querySelector('.c-qt__prog') as HTMLElement;
+		expect(progressBar?.style.width).toEqual('0%');
 	})
 
-    it('renders QuarterTracker in mid quarter', () => {
+	it('renders QuarterTracker in mid quarter', () => {
 		const { container } = render(<QuarterTracker date={moment("2006-02-01")} />)
 
 		// Check sign in button
 		const progressText = screen.getByText("Progress in Q1 '06");
-        expect(progressText).toBeTruthy();
+		expect(progressText).toBeTruthy();
 
-        const weeksText = screen.getByText("5 of 12");
-        expect(weeksText).toBeTruthy();
+		const weeksText = screen.getByText("5 of 12");
+		expect(weeksText).toBeTruthy();
 
-        const progressBar = container.querySelector('.c-qt__prog') as HTMLElement;
-        expect(progressBar?.style.width).toEqual('33%');
+		const progressBar = container.querySelector('.c-qt__prog') as HTMLElement;
+		expect(progressBar?.style.width).toEqual('33%');
 	})
 })
