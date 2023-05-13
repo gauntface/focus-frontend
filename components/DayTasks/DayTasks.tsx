7924bd0337eea5e95e940576a846e337c364f8c5
Matt Gaunt-Seo <matt@gaunt.dev>
2023-05-13 11:13:04 -0700

    Adding skeleton load

--- components/DayTasks/DayTasks.tsx
+++ components/DayTasks/DayTasks.tsx
@@ -2,7 +2,7 @@ import styles from './DayTasks.module.css';
 import { NotesArea } from '../../components/NotesArea/NotesArea';
 import { DailyPriority } from "../../models/priorities";
 
-export function DayTasks({priorities, notes, onNotesChange, onDailyPriorityChange}: Props) {
+export function DayTasks({priorities, notes, loading, onNotesChange, onDailyPriorityChange}: Props) {
 	return (
 		<div className={styles['c-dt__wrapper']}>
 			<div id="c-dt" className={styles['c-dt']}>
@@ -11,7 +11,7 @@ export function DayTasks({priorities, notes, onNotesChange, onDailyPriorityChang
 					<ol className={styles['c-dt__tasks-list']}>
 						{priorities.map((priority: DailyPriority, idx: number) => {
 							return (<li key={idx} className={styles['c-dt__task']}>
-								<NotesArea name={`priority-${idx}`} note={priority.note} onChange={(s: string) => onDailyPriorityChange(idx, s)} rows={1} />
+								<NotesArea name={`priority-${idx}`} note={priority.note} onChange={(s: string) => onDailyPriorityChange(idx, s)} rows={1} loading={loading} />
 							</li>);
 						})}
 					</ol>
@@ -20,7 +20,7 @@ export function DayTasks({priorities, notes, onNotesChange, onDailyPriorityChang
 				<section className={styles['c-dt__notes-section']}>
 					<h3>Notes</h3>
 					<div className={styles['c-dt__notes']}>
-						<NotesArea name={`notes`} note={notes}  onChange={onNotesChange} rows={3} />
+						<NotesArea name={`notes`} note={notes}  onChange={onNotesChange} rows={3} loading={loading} />
 					</div>
 				</section>
 			</div>
@@ -31,6 +31,7 @@ export function DayTasks({priorities, notes, onNotesChange, onDailyPriorityChang
 interface Props {
 	priorities: Array<DailyPriority>;
 	notes: string;
+	loading: boolean;
 	onNotesChange: (s: string) => void;
 	onDailyPriorityChange: (idx: number, s: string) => void;
 }
