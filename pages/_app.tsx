60350ef6d4f8d2fdd6b1bc7e4f1bb7bc49adbbce
Matt Gaunt-Seo <matt@gaunt.dev>
2023-06-25 10:24:08 -0700

    Add sentry (#103)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,23 @@ import { FocusAuthProvider } from '../contexts/Auth';
 import { ToastContainer } from "react-toastify";
 import { User } from "firebase/auth";
 import { NextComponentType } from "next";
+import * as Sentry from "@sentry/react";
 
+Sentry.init({
+	dsn: process.env.SENTRY_DSN,
+	integrations: [
+		new Sentry.BrowserTracing({
+			// Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+			tracePropagationTargets: ["localhost", /^https:\/\/*.gaunt.dev/],
+		}),
+		new Sentry.Replay(),
+	],
+	// Performance Monitoring
+	tracesSampleRate: 0.1,
+	// Session Replay
+	replaysSessionSampleRate: 0.1,
+	replaysOnErrorSampleRate: 1.0,
+});
 
 function FocusApp({
 	Component,
