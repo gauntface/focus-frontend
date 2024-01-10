b9aba886395aacdf5a9abe027d4b666ed6375954
Matt Gaunt-Seo <matt@gaunt.dev>
2024-01-09 19:39:59 -0800

    Simplify the week selector (#221)

--- __tests__/components/WeekSelector.test.tsx
+++ __tests__/components/WeekSelector.test.tsx
@@ -25,4 +25,20 @@ describe('WeekSelector', () => {
 		const textEle = await getByText('January 2006 - Week 1');
 		expect(textEle).toBeTruthy();
 	});
+
+	it('renders week selector correctly between years', async () => {
+		render(<WeekSelector date={new Date(2023, 11, 31)} />);
+
+		// Check sign in button
+		const prevWeekBtn = await getByLabelText('Previous week');
+		expect(prevWeekBtn).toBeTruthy();
+		expect(prevWeekBtn).toHaveAttribute('href', '/week/2023/52');
+
+		const nextWeekBtn = await getByLabelText('Next week');
+		expect(nextWeekBtn).toBeTruthy();
+		expect(nextWeekBtn).toHaveAttribute('href', '/week/2024/2');
+
+		const textEle = await getByText('January 2024 - Week 1');
+		expect(textEle).toBeTruthy();
+	});
 });
