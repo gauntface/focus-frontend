f48e4d14fe5a39c439e42eb891e85e5438c3edfc
Matt Gaunt-Seo <hello@gaunt.dev>
2024-01-09 19:24:07 -0800

    Simplify the week selector

--- components/WeekSelector/WeekSelector.tsx
+++ components/WeekSelector/WeekSelector.tsx
@@ -1,37 +1,22 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import {add, sub, format, getWeek, getYear} from 'date-fns';
+import {add, sub, format, getWeek, nextMonday, getWeekYear} from 'date-fns';
 
 import styles from "./WeekSelector.module.css";
 
 export function WeekSelector({date}: Props) {
-	// When dates cross years, the getWeek() will return the correct week
-	// (i.e. week 52 of year 2023 and week 1 of 2024) but getYear, may not
-	// return the correct year for the week.
-	// For example, Sunday 31st 2023 is week 1 of 2024, so getWeek(<sun 31st>)
-	// returns `1` but getYear(<sun 31st>) returns 2023.
-	// This naive approach returns the week + year we want
-	let prevWeek = getWeek(date) - 1;
-	let prevYear = getYear(date);
-	if (prevWeek <= 0) {
-		const lastWeek = sub(date, {weeks: 1});
-		prevWeek = getWeek(lastWeek);
-		prevYear = getYear(lastWeek);
-	}
+	const prevWeek = sub(date, {weeks: 1});
+	const nextWeek = add(date, {weeks: 1});
 
-	let nextYear = getYear(date);
-	let nextWeek = getWeek(date) + 1;
-	const nextWeekDate = add(date, {weeks: 1});
-	if (getWeek(nextWeekDate) === 1) {
-		nextWeek = 1;
-		nextYear += 1;
-	}
+	// Since the UI shows the first day as Monday, lets show as the date
+	const monday = nextMonday(date);
+
 	return (<div className={styles['c-ws']}>
-		<Link className={styles['c-ws__left']} href={`/week/${prevYear}/${prevWeek}`} aria-label="Previous week">
+		<Link className={styles['c-ws__left']} href={`/week/${getWeekYear(prevWeek)}/${getWeek(prevWeek)}`} aria-label="Previous week">
 			<Image width="8" height="14" src="/icons/arrow.svg" alt="Left arrow" />
 		</Link>
-		{format(date, 'MMMM yyyy')} - Week {getWeek(date)}
-		<Link className={styles['c-ws__right']} href={`/week/${nextYear}/${nextWeek}`} aria-label="Next week">
+		{format(monday, 'MMMM yyyy')} - Week {getWeek(date)}
+		<Link className={styles['c-ws__right']} href={`/week/${getWeekYear(nextWeek)}/${getWeek(nextWeek)}`} aria-label="Next week">
 			<Image width="8" height="14" src="/icons/arrow.svg" alt="Right arrow" />
 		</Link>
 	</div>);
