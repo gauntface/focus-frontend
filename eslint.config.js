0a8f2ccf9a52e57cf175fcce7c791da3ae4fee04
Matt Gaunt-Seo <hello@gaunt.dev>
2025-11-16 20:14:40 -0800

    Make eslint happier

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,11 @@ import { tanstackConfig } from "@tanstack/eslint-config";
 import { globalIgnores } from "eslint/config";
 
 export default [
-	globalIgnores(["old/", "take-screenshots.js"]),
+	globalIgnores([
+		"old/",
+		"take-screenshots.js",
+		"eslint.config.js",
+		"prettier.config.js",
+	]),
 	...tanstackConfig,
 ];
